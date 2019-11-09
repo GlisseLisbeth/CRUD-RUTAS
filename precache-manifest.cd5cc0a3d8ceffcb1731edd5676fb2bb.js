92220350d0cab2335918541e958e5b603fa216c8
glisselisbethjm <glisse1986.jm@outlook.com>
2019-11-09 00:10:57 -0500

    Updates

---
+++ precache-manifest.cd5cc0a3d8ceffcb1731edd5676fb2bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a8cc258afde0dc437a2453a940c2d1",
+    "url": "/CRUD-RUTAS/index.html"
+  },
+  {
+    "revision": "9981a1a48b2805228055",
+    "url": "/CRUD-RUTAS/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "9981a1a48b2805228055",
+    "url": "/CRUD-RUTAS/static/js/2.fe1f137a.chunk.js"
+  },
+  {
+    "revision": "f839e32f9339ca0c65eb",
+    "url": "/CRUD-RUTAS/static/js/main.4163eddf.chunk.js"
+  },
+  {
+    "revision": "e8f383e17ad3b67c5628",
+    "url": "/CRUD-RUTAS/static/js/runtime-main.d5feefed.js"
+  }
+]);
